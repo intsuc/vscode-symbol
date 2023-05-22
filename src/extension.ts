b1c2931672a3bcfbd0d7af3d00490e41a0e2c216
intsuc <intsuc@gmail.com>
2023-05-22 15:51:26 +0900

    Implement

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,24 @@
-import { ExtensionContext } from "vscode";
+import { ExtensionContext, ProviderResult, SymbolInformation, SymbolKind, languages } from "vscode";
 
 export function activate(context: ExtensionContext) {
-
-
-	context.subscriptions.push();
+	context.subscriptions.push(
+		languages.registerDocumentSymbolProvider(
+			{ scheme: "file" },
+			{
+				provideDocumentSymbols(document, _token): ProviderResult<SymbolInformation[]> {
+					return [{
+						name: document.fileName,
+						containerName: document.fileName,
+						kind: SymbolKind.File,
+						location: {
+							uri: document.uri,
+							range: document.lineAt(0).range
+						}
+					}];
+				},
+			}
+		)
+	);
 }
 
 export function deactivate() { }
